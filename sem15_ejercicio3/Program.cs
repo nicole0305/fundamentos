0d49352e03c3fe1d23821e970f08b9376901ad99
UPN <UPN@ULCLAB421>
2024-11-22 12:21:55 -0500

    ejer3

---
+++ sem15_ejercicio3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem15_ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese # filas: ");
+            int f = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese # columnas: ");
+            int c = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[f, c];
+            int[] suma = new int[c];
+            Random rnd = new Random();
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = rnd.Next(30, 51);
+                    suma[j] += matriz[i, j];
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            for (int i=0; i < c; i++)
+            {
+                Console.Write(suma[i] + "\t");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
